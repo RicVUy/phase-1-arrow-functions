fa5b325a3b754976d2c0392e582b19a06529ceaa
RicardoUy <rvuy62@yahoo.com>
2023-03-23 13:31:05 -0500

    submit arrow function

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(a,b) {
+    return a/b;
+}
+divide(a,b);
+let x = 2;
+const square = x => x ** 2;
+ square(x);
+let a = 3;
+let b = 4;
+const add = (a,b) => a + b;
+add(a,b);
